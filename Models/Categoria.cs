ed7984056aacf2bac8fda787df554e31db2a20d8
EDNALDO santos <joseednaldo1983@gmail.com>
2020-05-22 21:01:00 -0300

    Add project files.

---
+++ Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        [StringLength(100)]
+        public string CategoriaNome { get; set; }
+
+        [StringLength(250)]
+        public string Descricao { get; set; }
+        public List<Lanche> Lanches { get; set; }  // relacionamnetoi 1:N  (1 pra muitos)
+
+    }
+}
