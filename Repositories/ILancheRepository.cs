ce6e50cff8f209aa1fb0cffa6ab7f2c8bd4de690
EDNALDO santos <joseednaldo1983@gmail.com>
2020-05-22 21:14:31 -0300

    Ciração de interfaces

---
+++ Repositories/ILancheRepository.cs
@@ -0,0 +1,15 @@
+﻿using LanchesMac.Models;
+using System.Collections.Generic;
+
+namespace LanchesMac.Repositories
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPrefferido { get; }
+
+        Lanche GetLancheById(int lancheId);
+        
+
+    }
+}
